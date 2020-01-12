5daf8a3e716e7eb01cfd8bd246507e4b80c9a4ef
root <kenneth.crimson@gmail.com>
2020-01-12 14:05:53 +0000

    added mongodb atlas

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ const indexRoutes = require("./routes/index"),
 // General config
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://doc_keith:banken@cluster0-rbc7x.mongodb.net/test?retryWrites=true&w=majority", { 
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR", err.message);
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
